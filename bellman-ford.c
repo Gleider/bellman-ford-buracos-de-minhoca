42ee116cb5431d48935b03e48a2c08b03dd48f4d
Gleider <gleider.ec@gmail.com>
2017-03-07 00:13:05 -0300

    inicialização das variáveis

--- bellman-ford.c
+++ bellman-ford.c
@@ -18,3 +18,22 @@ struct grafo{
 }typedef Grafo;
 
 //------------------------------------
+
+int main(){
+	int numTeste; 	// quantidade de conjuntos a serem testados
+	int n;			// n => número de galáxias
+	int m;			// m => número de buracos de minhoca
+	int x;			// x => galáxia que está a entrada do buraco
+	int y;			// y => galáxia que está a saída do buraco
+	int t;			// t =>	diferença de tempo, em anos, entre entrada e saída
+	
+	scanf("%d", &numTeste);
+	while(numTeste > 0){
+		scanf("%d %d %d %d %d", &n, &m, &x, &y, &t);
+		
+		numTeste--;
+	}
+	
+	
+	return 0;
+}
